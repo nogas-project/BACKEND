cd5ebceac43b28393e038e478046d1fbb30e4a64
Yves-Shaheem <shaheemshedid4@gmail.com>
2025-02-03 17:48:54 -0500

    user modify service method almost done

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,5 +5,6 @@ dotenv.config();
 export const config = {
     PORT: process.env.PORT || 3001,
     ENV: process.env.NODE_ENV || 'development',
-    firebasePath: process.env.SERVICE_ACCOUNT_PATH
+    firebasePath: process.env.SERVICE_ACCOUNT_PATH,
+    JWT_SECRET: process.env.JWT_SECRET || "JWT_SECRET_NOGAS_TOKEN",
 }
