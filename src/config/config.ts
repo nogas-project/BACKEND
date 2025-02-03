30ed405f2ebd4f2c3bbcb26f5ba7ea3c28a803da
Yves-Shaheem <shaheemshedid4@gmail.com>
2025-02-03 14:01:43 -0500

    change port

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-    PORT: process.env.PORT || 3000,
+    PORT: process.env.PORT || 3001,
     ENV: process.env.NODE_ENV || 'development',
     firebasePath: process.env.SERVICE_ACCOUNT_PATH
 }
